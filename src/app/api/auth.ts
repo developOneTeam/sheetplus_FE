1edc085f3783a9a29b9bb4ca752ec6f02b5753dc
paperbox <7126454+lego37yoon@users.noreply.github.com>
2024-10-09 03:55:08 +0900

    feat(auth): sending mail test

--- src/app/api/auth.ts
+++ src/app/api/auth.ts
@@ -1,8 +1,16 @@
 import { D1Adapter } from "@auth/d1-adapter";
 import { getRequestContext } from "@cloudflare/next-on-pages";
 import NextAuth from "next-auth";
+import Mailgun from "next-auth/providers/mailgun";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [],
+    providers: [
+        Mailgun({
+            apiKey: process.env.AUTH_MAILGUN_KEY,
+            from: "schusheetp@mail.mysw.moe"
+        })
+    ],
     adapter: D1Adapter(getRequestContext().env.TEST_DB),
-})
+});
+
+export const runtime = 'edge';
