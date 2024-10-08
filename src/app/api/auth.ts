62f4e74a4e6d422e50c19e46de36eb4d29781f24
paperbox <7126454+lego37yoon@users.noreply.github.com>
2024-10-09 02:22:49 +0900

    feat(auth): prepare for testing auth with d1 database

--- src/app/api/auth.ts
+++ src/app/api/auth.ts
@@ -1,5 +1,8 @@
+import { D1Adapter } from "@auth/d1-adapter";
+import { getRequestContext } from "@cloudflare/next-on-pages";
 import NextAuth from "next-auth";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [],
+    adapter: D1Adapter(getRequestContext().env.TEST_DB),
 })
