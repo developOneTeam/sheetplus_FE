ab5d2f31cd3ba9d4c049a1bc1833cf57edf4783e
paperbox <7126454+lego37yoon@users.noreply.github.com>
2024-10-09 03:59:34 +0900

    fix(auth): move file location to avoid top-level-grc error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { signIn } from "./api/auth";
+import { signIn } from "../auth";
 
 export default function Home() {
   return (
