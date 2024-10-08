b882bea6099b18c8da74c5c27ae22adcca2a9ff5
paperbox <7126454+lego37yoon@users.noreply.github.com>
2024-10-09 04:11:25 +0900

    fix(auth): fix top-level error using another solution
    - reference: https://github.com/nextauthjs/next-auth/issues/11423

--- src/auth.ts
+++ src/auth.ts
@@ -3,14 +3,18 @@ import { getRequestContext } from "@cloudflare/next-on-pages";
 import NextAuth from "next-auth";
 import Mailgun from "next-auth/providers/mailgun";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [
-        Mailgun({
-            apiKey: process.env.AUTH_MAILGUN_KEY,
-            from: "schusheetp@mail.mysw.moe"
-        })
-    ],
-    adapter: D1Adapter(getRequestContext().env.TEST_DB),
+export const { handlers, signIn, signOut, auth } = NextAuth(() => {
+    const db = getRequestContext().env.TEST_DB;
+    
+    return {
+        providers: [
+            Mailgun({
+                apiKey: process.env.AUTH_MAILGUN_KEY,
+                from: "schusheetp@mail.mysw.moe"
+            })
+        ],
+        adapter: D1Adapter(db),
+    }
 });
 
 export const runtime = 'edge';
