79ad8b9ad889a835cbc43d3a5d00d3298f91a17e
paperbox <7126454+lego37yoon@users.noreply.github.com>
2024-10-09 22:41:18 +0900

    feat(style): automatically enable/disable mail-send btn

---
+++ src/app/components/LoginForm.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { InputWrapper, Input, Button } from "../styles/ui";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
+
+export default function LoginForm() {
+    const [formDisabled, disableForm] = useState<"disabled"|undefined>("disabled");
+    const submitButton = useRef<HTMLButtonElement>(null);
+
+    function enableButton(e: ChangeEvent<HTMLInputElement>) {
+        if (e.target.value.length === 0) {
+            disableForm("disabled");
+        } else {
+            disableForm(undefined);
+        }
+    }
+
+    useEffect(() => {
+        if (formDisabled && submitButton.current) {
+            submitButton.current.removeAttribute("disabled");
+        }
+    }, [formDisabled]);
+    
+    return (
+        <>
+          <InputWrapper>
+            <span className="material-symbols-rounded">mail</span>
+            <Input type="text" name="email" placeholder="Email" onChange={enableButton} />
+            <label htmlFor="email">@sch.ac.kr</label>
+          </InputWrapper>
+          <Button ref={submitButton} type="submit" types={formDisabled} disabled>메일 보내기</Button>
+        </>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,47 @@
-import { signIn } from "../auth";
+import { AccentArea, FaqLink, Form, Header, IconButton, IconDesc, Main } from "./styles/ui";
+import "./page.css";
+import LoginForm from "./components/LoginForm";
+import { signIn } from "@/auth";
 
 export default function Home() {
+
+
   return (
-    <form action={async(formData) => {
-      "use server"
-      formData.set("email", `${formData.get("email")}@sch.ac.kr`);
-      await signIn("mailgun", formData);
-    }}>
-      <input type="text" name="email" placeholder="Email" />
-      <label htmlFor="email">@sch.ac.kr</label>
-      <button type="submit">Sign In</button>
-    </form>
+    <>
+      <Header>
+        <h1 className="title">
+          SCHU<span className="sheet">sheet<sup>+</sup></span>
+          <span className="subtitle">제1회 SW융합대학 학술제</span>
+        </h1>
+        <section className="icon-nav">
+          <IconButton className="material-symbols-rounded">
+            notifications
+          </IconButton>
+        </section>
+      </Header>
+      <Main center>
+        <section>
+          <IconDesc>
+            <span className="material-symbols-rounded">
+              forward_to_inbox
+            </span>
+            <h2>이메일로 입장하기</h2>
+            <p>SW융합대학 학부생만 참여할 수 있어요</p>
+          </IconDesc>
+          <Form action={async(formData) => {
+            "use server"
+            formData.set("email", `${formData.get("email")}@sch.ac.kr`);
+            await signIn("mailgun", formData);
+          }}>
+            <LoginForm />
+          </Form>
+          <AccentArea center size="sm">
+            <p>이메일 주소를 입력하시면 <br /> 로그인 혹은 가입할 수 있는 링크를 보내드려요.</p>
+          </AccentArea>
+          <FaqLink href="/">이메일 주소가 기억나지 않나요?</FaqLink>
+        </section>
+      </Main>
+    </>
   );
 }
 
