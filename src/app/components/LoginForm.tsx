b2b7af3a007d2f8051e84b85ffa4cbe26b67f9f0
paperbox <7126454+lego37yoon@users.noreply.github.com>
2024-10-11 01:22:10 +0900

    feat(auth): mail address style validation

--- src/app/components/LoginForm.tsx
+++ src/app/components/LoginForm.tsx
@@ -4,14 +4,22 @@ import { ChangeEvent, useEffect, useRef, useState } from "react";
 
 export default function LoginForm() {
     const [formDisabled, disableForm] = useState<"disabled"|undefined>("disabled");
+    const [formMessage, setFormMessage] = useState<string>("");
     const submitButton = useRef<HTMLButtonElement>(null);
 
     function enableButton(e: ChangeEvent<HTMLInputElement>) {
-        if (e.target.value.length === 0 && submitButton.current) {
+        const isFullAddress = e.target.value.includes("@");
+
+        if ((e.target.value.length === 0 || isFullAddress)
+            && submitButton.current) {
             disableForm("disabled");
             submitButton.current.setAttribute("disabled", "");
         } else {
             disableForm(undefined);
+        }
+
+        if (isFullAddress) {
+            setFormMessage("순천향대 메일 주소를 아이디만 입력해주세요.");
         }
     }
 
@@ -29,6 +37,7 @@ export default function LoginForm() {
             <label htmlFor="email">@sch.ac.kr</label>
           </InputWrapper>
           <Button ref={submitButton} type="submit" types={formDisabled} disabled>메일 보내기</Button>
+          <p>{formMessage}</p>
         </>
     );
 }
