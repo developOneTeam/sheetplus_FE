2d0be97a10758bb201c784e1d1c4ece529221f49
paperbox <7126454+lego37yoon@users.noreply.github.com>
2024-10-17 01:03:37 +0900

    feat(work): add notice for works page

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,6 +1,18 @@
+import { iconDesc, main, mainSection } from "../styles/layouts.css";
+import { icon, defaultH2, defaultP } from "../styles/others.css";
+
 export default function Page() {
     return (
-        <>
-        </>
+        <main className={main({ center: true })}>
+            <section className={mainSection}>
+                <div className={iconDesc}>
+                    <span className={`${icon({ color: "disabled" })} material-symbols-rounded`}>
+                    deployed_code_history
+                    </span>
+                    <h2 className={defaultH2({ style: "disabled" })}>준비 중이에요</h2>
+                    <p className={defaultP({ style: "disabled" })}>작품 탐색 기능을 준비하고 있어요</p>
+                </div>
+            </section>
+        </main>
     )
 };
