5c942c126cbde6c4d441428519d856a78ba9e7e6
paperbox <7126454+lego37yoon@users.noreply.github.com>
2024-10-17 02:39:58 +0900

    fix(style): duplicated elements in header and nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Nav from "@/app/components/Nav";
-import { iconButton } from "./styles/buttons.css";
-import { header, title, sheet, subtitle, iconNav } from "./styles/layouts.css";
-import { icon } from "./styles/others.css";
-import Link from "next/link";
-import Logout from "./components/Logout";
 
 const suiteV = localFont({
   src: "./fonts/SUITE-Variable.woff2",
@@ -28,22 +22,7 @@ export default async function RootLayout({
   return (
     <html lang="ko">
       <body className={suiteV.className}>
-        <header className={header()}>
-          <h1>
-            <Link href="/" className={title}>
-              SCHU<span className={sheet}>sheet<sup>+</sup></span>
-              <span className={subtitle}>제1회 SW융합대학 학술제</span>
-            </Link>
-          </h1>
-          <section className={iconNav}>
-            <Logout />
-            <Link href="/notifications" className={`${iconButton()} ${icon} material-symbols-rounded`}>
-              notifications
-            </Link>
-          </section>
-        </header>
         {children}
-        <Nav />
       </body>
     </html>
   );
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,18 +2,18 @@ import Link from 'next/link'
 import { iconDesc, main } from './styles/layouts.css'
 import { defaultH2, defaultP, icon } from './styles/others.css'
 import { button } from './styles/buttons.css'
- 
+
 export default function NotFound() {
-  return (
-    <main className={main({ center: true })}>
-        <section className={iconDesc}>
-          <span className={`${icon({ color: "error" })} material-symbols-rounded`}>
-          live_help
-          </span>
-          <h2 className={defaultH2()}>페이지를 찾을 수 없어요</h2>
-          <p className={defaultP()}>찾으시는 주소가 맞는지 다시 한 번 확인해주세요</p>
-          <Link href="/" className={button({ types: "secondary" })}>홈으로 돌아가기</Link>  
-        </section>
-    </main>
-  );
+    return (
+        <main className={main({ center: true })}>
+                <section className={iconDesc}>
+                    <span className={`${icon({ color: "error" })} material-symbols-rounded`}>
+                        live_help
+                    </span>
+                    <h2 className={defaultH2()}>페이지를 찾을 수 없어요</h2>
+                    <p className={defaultP()}>찾으시는 주소가 맞는지 다시 한 번 확인해주세요</p>
+                    <Link href="/" className={button({ types: "secondary" })}>홈으로 돌아가기</Link>
+                </section>
+        </main>
+    );
 };
