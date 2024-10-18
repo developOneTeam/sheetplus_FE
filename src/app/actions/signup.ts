a2f658f07646b6ec909360690c2cdeb929f25bdb
paperbox <7126454+lego37yoon@users.noreply.github.com>
2024-10-18 13:46:34 +0900

    feat(auth): signup action sets cookies for access/refresh tokens

--- src/app/actions/signup.ts
+++ src/app/actions/signup.ts
@@ -1,6 +1,10 @@
 "use server"
 
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+
 export async function Login(status: { ok: boolean, try: number }, formData:FormData) {
+    const cookieBox = cookies()
 
     const result = await fetch(`${process.env.API_ENDPOINT}/public/mail/auth`, {
         method: "POST",
@@ -16,9 +20,19 @@ export async function Login(status: { ok: boolean, try: number }, formData:FormD
         })
     });
 
-    if (result.ok)
+    if (result.ok) {
         status.ok = true;
-    else
+        const tokens: {
+            data: {
+                accessToken: string,
+                refreshToken: string
+            }
+        } = await result.json();
+        cookieBox.set("access", tokens.data.accessToken);
+        cookieBox.set("refresh", tokens.data.refreshToken);
+
+        redirect("/")
+    } else
         status.ok = false;
 
     status.try += 1
