5ef3f73dc75aa17fa90bd77ddeaa564fe92997b7
paperbox <7126454+lego37yoon@users.noreply.github.com>
2024-10-21 14:52:57 +0900

    debug(auth): check error message of production

--- src/app/actions/login.ts
+++ src/app/actions/login.ts
@@ -61,6 +61,8 @@ export async function Login(status: { ok: boolean, try: number, notSelected: boo
         else
             status.ok = false;
     
+        console.log(await result.text());
+
         status.try += 1
     
         return status;    
