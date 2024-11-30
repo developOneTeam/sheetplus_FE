754b2ae0011e7b490897733cca4704df3e3dbaaa
paperbox <7126454+lego37yoon@users.noreply.github.com>
2024-12-01 00:01:26 +0900

    feat(pwa): update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,15 @@ import "./globals.css";
 const suiteV = localFont({
   src: "./fonts/SUITE-Variable.woff2",
   variable: "--font-suite-v",
-  weight: "300 900",
+  weight: "300 900"
 });
 
 export const metadata: Metadata = {
   title: "SCHU Sheet+",
   description: "순천향대학교 학부생을 위한 학술제 서비스",
+  icons: {
+    icon: "/favicon.png"
+  }
 };
 
 export default async function RootLayout({
