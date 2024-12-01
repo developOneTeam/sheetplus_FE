e9594cbee29f74242adb8e5c71a2c8a36ab08d7a
paperbox <7126454+lego37yoon@users.noreply.github.com>
2024-12-01 23:51:00 +0900

    feat(schedule); add default schedule page layout

--- src/app/(student)/[contest]/schedule/page.tsx
+++ src/app/(student)/[contest]/schedule/page.tsx
@@ -1,6 +1,26 @@
-export default function Page() {
+import ContentsTable from "@/app/components/ContentsTable";
+
+export default async function Page({ params } : { params: { contest : string } }) {
+
+    let schedule = null;
+
+    const fetchData = await fetch(`${process.env.API_ENDPOINT}/public/${params.contest}/schedule`, {
+        method: "get",
+        headers: {
+            "Content-Type" : "application/json",
+        }
+    });
+
+    if (fetchData.ok) {
+        schedule = await fetchData.json();
+    }
+
     return (
         <>
+        
+        {schedule && schedule.data.length > 0 ? 
+            <ContentsTable type="schedule" data={schedule.data} />
+        :""}
         </>
     )
 };
